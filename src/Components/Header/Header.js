fb210e1e55ab55c3ec4ee3f11c437ba3dc80b144
Aser <Dr@Asers-MacBook-Pro.local>
2018-08-09 00:17:16 -0400

    minor additions

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,6 +9,10 @@ export default class Header extends Component {
         <header>
 
     <span className='logo'><img src='https://vignette.wikia.nocookie.net/rickandmorty/images/1/1e/Rick_and_morty_icon.png/revision/latest?cb=20150805041642&format=original' /></span>
+    
+    {/* This won't work unless you do a hack that i'm not going to research. Typically
+        Images would be retrieved from another server, not locally I'm seeing. (May be wrong) */}
+    {/* <span className='logo'><img src={'Rm_icon.png'} /></span> */}
 
       <ul className='nav'>
         <li className='nav-items'>Home</li>
