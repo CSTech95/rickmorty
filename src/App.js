c93a8eb98d9e75490a95d382996fa9589522c182
Aser <Dr@Asers-MacBook-Pro.local>
2018-08-19 03:07:25 -0400

    Modulized Code

--- src/App.js
+++ src/App.js
@@ -1,42 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import './App.css';
-import axios from 'axios';
-import Characters from './Components/Characters/Characters';
-import Header from './Components/Header/Header';
-import S_Characters from './Components/Characters/S_Characters/S_Characters';
-
+import axios from "axios";
+import Characters from "./Components/Characters/Characters";
+import Header from "./Components/Header/Header";
+import S_Characters from "./Components/Characters/S_Characters/S_Characters";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      persons:[]
-    }
-  }
-  componentDidMount() {
-    const id = 1;
-    axios.get(`https://rickandmortyapi.com/api/character/?page=${id}`)
-      .then(res => {
-        const persons = res.data.results;
-        // console.log(persons);
-        this.setState({ persons:persons });
-        console.log(this.state.persons);
-      })
-  }
   render() {
     return (
       <div>
         <Header />
-        <S_Characters>
-        { this.state.persons.map(x => 
-          <Characters 
-          name={x.name} 
-          status={x.status} 
-          image={x.image}
-          />
-          
-        ) }
-        </S_Characters>
+        <Characters />
       </div>
     );
   }
--- src/Components/Characters/S_Characters/S_Characters.js
+++ src/Components/Characters/S_Characters/S_Characters.js
@@ -1,29 +1,24 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const myRed = '#fb595d';
-const myBlue = '#9FCDE6';
+const myRed = "#fb595d";
+const myBlue = "#9FCDE6";
 
 export default styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: repeat(3, 1fr);
-    align-items: center;
-    justify-items: center;
-    background: lightcoral;
-    grid-gap: 15px;
+  display: grid;
+  grid-template-columns: repeat(3, auto);
+  grid-template-rows: repeat(3, auto);
+  align-items: center;
+  justify-items: center;
+  background: lightcoral;
+  grid-gap: 15px;
+  text-align: center;
 
-    li{
-        list-style-type: none;
-        text-align: center;
-    }
+  li {
+    list-style-type: none;
+  }
 
-
-
-
-
-
-
-
-
-`
+  img {
+    width: 100%;
+  }
+`;
