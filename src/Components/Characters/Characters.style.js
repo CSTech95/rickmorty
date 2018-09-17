cdd877f0f6f75ab6e49c0bcd890427e0762c61ed
Aser <Dr@Asers-MacBook-Pro.local>
2018-09-17 16:58:57 -0400

    ...

--- src/Components/Characters/Characters.style.js
+++ src/Components/Characters/Characters.style.js
@@ -7,7 +7,7 @@ const myBlue = "#9FCDE6";
 export const Div = styled.div`
   height: 100px;
   width: 105px;
-  border: 1.5px solid grey;
+  border: 1.5px solid black;
   box-shadow: 5px 5px 5px black;
   display: grid;
   grid-template-rows: 1fr 4fr 1fr;
