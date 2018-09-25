734823493c4c51bbdbbb2518bc73d805863793fe
Aser <techdr95@gmail.com>
2018-09-25 19:19:18 -0400

    Changed footer color

--- src/Components/Footer/Footer.style.js
+++ src/Components/Footer/Footer.style.js
@@ -2,12 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import { colors } from "../Colors";
 
-const { red, blue, black, grey, lightblue } = colors;
+const { red, blue, black, grey, lightblue, darkblue } = colors;
 
 export const Myfooter = styled.footer`
-  background: ${lightblue};
+  background: ${darkblue};
   color: ${red};
-  border-top: 1px solid ${red};
+  /* border-top: 1px solid ${red}; */
   width: 100%;
   height: 45px;
   bottom: 0px;
