cb86bc1af99ed8285b34e1e67c23fd0b41e05a9f
Aser <techdr95@gmail.com>
2018-09-25 01:58:08 -0400

    ...

--- src/Components/Character/Character.js
+++ src/Components/Character/Character.js
@@ -3,10 +3,10 @@ import { Img, Div } from "./Character.style";
 const Character = props => {
   return (
     <Div>
-      {/* <h5>
+      <h5>
         {props.name.slice(0, 10)}
         ...
-      </h5> */}
+      </h5>
       <Img src={props.image} />
       {/* <h5>{props.status}</h5> */}
     </Div>
